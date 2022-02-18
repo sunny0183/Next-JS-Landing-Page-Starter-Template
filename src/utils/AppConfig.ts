1920bea8568a466a9774ba7b8328a241351b5c9c
sunny0183 <mohammed.asifuzzaman@hotmail.com>
2022-02-18 10:59:42 +0000

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Zaman Consultancy Limited',
+  title: 'Zaman Consultancy Limited',
+  description:
+    'Zaman Consultancy Limited is a leading Microsoft 365, M365, Azure, Dynamics 365 Partner and MSP based in London. Our mission is to help clients across the UK succeed through transformative Technology, Solutions and Services.',
   locale: 'en',
 };
